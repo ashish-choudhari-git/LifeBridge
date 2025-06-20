e20d33d07abcbfa863e81ec4ac29af2a0213f2b1
Yachinverma <yachinverma@gmail.com>
2025-06-20 13:06:46 +0530

    toastify integration done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'
-import Availability from './pages/Availability/Availability.jsx'
-import Donate from './pages/Donate/Donate.jsx'
-
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Availability from './pages/Availability/Availability.jsx';
+import Donate from './pages/Donate/Donate.jsx';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -13,17 +14,26 @@ function App() {
 
       <div style={{ paddingTop: '4rem' }}>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/availability" element={<Availability />}></Route>
-          <Route path="/donate" element={<Donate />}></Route>
-          {/* <Route path="/Bank" element={<Donate />}></Route> */}
-
-
+          <Route path="/" element={<Home />} />
+          <Route path="/availability" element={<Availability />} />
+          <Route path="/donate" element={<Donate />} />
         </Routes>
       </div>
 
+      {/* Toastify container */}
+      <ToastContainer 
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
